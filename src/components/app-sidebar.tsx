f2823e8acd9db7cf8c6d61d65892b2a38a18924d
kubaplata <kubaplata@icloud.com>
2024-12-11 21:37:16 +0100

    fix build errors

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,19 +3,14 @@ import {
   BookOpen,
   Bot,
   Command,
-  Frame,
   LifeBuoy,
-  Map,
-  PieChart,
   Send,
   Settings2,
   SquareTerminal,
 } from "lucide-react"
 
 import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
 import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
   SidebarContent,
